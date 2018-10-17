5a6f02a03f5ddf0ea77ebe828f40b2e36827f8ee
monkemedia <info@monkemedia.co.uk>
2018-10-17 20:48:54 +0100

    Added contact page

---
+++ pages/contact-us.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+  div.homepage(ref="homepage")
+    header-homepage(:data="homepage")
+    .main
+      content-blocks(:data="homepage.contentBlocks")
+      banner(:data="homepage.banner")
+      section
+        .container.sub-main
+          featured-items(:data="homepage.featuredItems")
+          content-asset(:data="homepage")
+
+</template>
+
+<script>
+  import HeaderHomepage from '@/components/Headers/HeaderHomepage.vue'
+  import ContentBlocks from '@/components/Homepage/ContentBlocks/Index.vue'
+  import Banner from '@/components/Homepage/Banner/Index.vue'
+  import FeaturedItems from '@/components/Homepage/FeaturedItems/Index.vue'
+  import ContentAsset from '@/components/Homepage/ContentAsset/Index.vue'
+
+  export default {
+    components: {
+      HeaderHomepage,
+      ContentBlocks,
+      Banner,
+      FeaturedItems,
+      ContentAsset
+    },
+
+    async fetch ({ store }) {
+      return store.dispatch('homepage/fetchData')
+    },
+
+    mounted () {
+      if (!process.client) return
+
+      return this.$store.dispatch('homepage/fetchData')
+    },
+
+    computed: {
+      homepage () {
+        return this.$store.getters['homepage/getData']
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
