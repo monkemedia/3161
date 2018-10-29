99141eb1af0469dffec01454a240928efd0e233c
monkemedia <info@monkemedia.co.uk>
2018-10-29 19:25:40 +0000

    Contact form now has a success message

--- components/Contact/ContactForm.vue
+++ components/Contact/ContactForm.vue
@@ -1,98 +1,102 @@
 <template lang="pug">
-  form(@submit.prevent="submit")
-    .notification.is-danger.is-error.has-text-centered(v-if="isSignInError") {{ isSignInError }}
-    .columns
-      .column
-        .field(:class="{ 'is-error': errors.has('reason') }")
-          label.label Select a reason for your message #[sup *]
-          .control.is-expanded
-            .select.is-fullwidth.is-multiple
-              .select-icon
-                fa.icon(:icon="['fas', 'caret-down']")
-              select(
-              name="reason"
-              id="reason"
-              v-model="formData.reason"
-              data-vv-delay="600"
-              :class="{ 'is-danger': errors.has('reason') }"
-              v-validate="'required'")
-                option(disabled value="") Select a reason for your message
-                option(v-for="reason in reasons" :value="reason.code") {{ reason.label }}
-            p(v-show="errors.has('reason')" class="help is-danger" v-html="'Whoops! ' + errors.first('reason')")
-    .columns
-      .column.is-6
-        .field(:class="{ 'is-error': errors.has('firstName') }")
-          label.label First name #[sup *]
-          .control
-            input.input(
-              name="firstName"
-              id="firstName"
-              v-model="formData.first_name"
-              type="text"
-              data-vv-delay="600"
-              :class="{ 'is-danger': errors.has('firstName') }"
-              v-validate="'required'")
-            p(v-show="errors.has('firstName')" class="help is-danger" v-html=" 'Whoops! ' + errors.first('firstName')")
-      .column.is-6
-        .field(:class="{ 'is-error': errors.has('lastName') }")
-          label.label Last name #[sup *]
-          .control
-            input.input(
-              name="lastName"
-              id="lastName"
-              v-model="formData.last_name"
-              type="text"
-              data-vv-delay="600"
-              :class="{ 'is-danger': errors.has('lastName') }"
-              v-validate="'required'")
-            p(v-show="errors.has('lastName')" class="help is-danger" v-html="'Whoops! ' +  errors.first('lastName')")
-    .columns
-      .column.is-6
-        .field(:class="{ 'is-error': errors.has('email') }")
-          label.label Email #[sup *]
-          .control
-            input.input(
-              name="email"
-              id="email"
-              v-model="formData.email"
-              type="email"
-              data-vv-delay="600"
-              :class="{ 'is-danger': errors.has('email') }"
-              v-validate="'required|email'")
-            p(v-show="errors.has('email')" class="help is-danger" v-html="'Whoops! ' + errors.first('email')")
-      .column.is-6
-        .field(:class="{ 'is-error': errors.has('phoneNumber') }")
-          label.label Phone number
-          .control
-            input.input(
-              name="phoneNumber"
-              id="phoneNumber"
-              v-model="formData.phone_number"
-              type="telephone"
-              data-vv-delay="600"
-              :class="{ 'is-danger': errors.has('phoneNumber') }"
-              v-validate="'phoneNumber'")
-            p(v-show="errors.has('phoneNumber')" class="help is-danger" v-html="'Whoops! ' + errors.first('phoneNumber')")
-    .columns
-      .column
-        .field(:class="{ 'is-error': errors.has('message') }")
-          label.label Message #[sup *]
-          .control
-            textarea.textarea(
-              name="message"
-              id="message"
-              v-model="formData.message"
-              type="text"
-              data-vv-delay="600"
-              :class="{ 'is-danger': errors.has('message') }"
-              v-validate="'required'")
-            p(v-show="errors.has('message')" class="help is-danger" v-html="'Whoops! ' + errors.first('message')")
-    .columns
-      .column
-        .field.is-grouped.is-grouped-right
-          .control
-            button.button.is-primary.is-flip(:class="{ 'is-loading': isLoading }" type="submit")
-              span(data-text="Send message") Send message
+  div
+    .success-container.is-success(v-if="isSuccess")
+      h3.title Your message has been sent!
+      p {{ formData.first_name }} we will contact you as soon as possible.
+    form(@submit.prevent="submit" v-else)
+      .notification.is-danger.is-error.has-text-centered(v-if="isSignInError") {{ isSignInError }}
+      .columns
+        .column
+          .field(:class="{ 'is-error': errors.has('reason') }")
+            label.label Select a reason for your message #[sup *]
+            .control.is-expanded
+              .select.is-fullwidth.is-multiple
+                .select-icon
+                  fa.icon(:icon="['fas', 'caret-down']")
+                select(
+                name="reason"
+                id="reason"
+                v-model="formData.reason"
+                data-vv-delay="600"
+                :class="{ 'is-danger': errors.has('reason') }"
+                v-validate="'required'")
+                  option(disabled value="") Select a reason for your message
+                  option(v-for="reason in reasons" :value="reason.code") {{ reason.label }}
+              p(v-show="errors.has('reason')" class="help is-danger" v-html="'Whoops! ' + errors.first('reason')")
+      .columns
+        .column.is-6
+          .field(:class="{ 'is-error': errors.has('firstName') }")
+            label.label First name #[sup *]
+            .control
+              input.input(
+                name="firstName"
+                id="firstName"
+                v-model="formData.first_name"
+                type="text"
+                data-vv-delay="600"
+                :class="{ 'is-danger': errors.has('firstName') }"
+                v-validate="'required'")
+              p(v-show="errors.has('firstName')" class="help is-danger" v-html=" 'Whoops! ' + errors.first('firstName')")
+        .column.is-6
+          .field(:class="{ 'is-error': errors.has('lastName') }")
+            label.label Last name #[sup *]
+            .control
+              input.input(
+                name="lastName"
+                id="lastName"
+                v-model="formData.last_name"
+                type="text"
+                data-vv-delay="600"
+                :class="{ 'is-danger': errors.has('lastName') }"
+                v-validate="'required'")
+              p(v-show="errors.has('lastName')" class="help is-danger" v-html="'Whoops! ' +  errors.first('lastName')")
+      .columns
+        .column.is-6
+          .field(:class="{ 'is-error': errors.has('email') }")
+            label.label Email #[sup *]
+            .control
+              input.input(
+                name="email"
+                id="email"
+                v-model="formData.email"
+                type="email"
+                data-vv-delay="600"
+                :class="{ 'is-danger': errors.has('email') }"
+                v-validate="'required|email'")
+              p(v-show="errors.has('email')" class="help is-danger" v-html="'Whoops! ' + errors.first('email')")
+        .column.is-6
+          .field(:class="{ 'is-error': errors.has('phoneNumber') }")
+            label.label Phone number
+            .control
+              input.input(
+                name="phoneNumber"
+                id="phoneNumber"
+                v-model="formData.phone_number"
+                type="telephone"
+                data-vv-delay="600"
+                :class="{ 'is-danger': errors.has('phoneNumber') }"
+                v-validate="'phoneNumber'")
+              p(v-show="errors.has('phoneNumber')" class="help is-danger" v-html="'Whoops! ' + errors.first('phoneNumber')")
+      .columns
+        .column
+          .field(:class="{ 'is-error': errors.has('message') }")
+            label.label Message #[sup *]
+            .control
+              textarea.textarea(
+                name="message"
+                id="message"
+                v-model="formData.message"
+                type="text"
+                data-vv-delay="600"
+                :class="{ 'is-danger': errors.has('message') }"
+                v-validate="'required'")
+              p(v-show="errors.has('message')" class="help is-danger" v-html="'Whoops! ' + errors.first('message')")
+      .columns
+        .column
+          .field.is-grouped.is-grouped-right
+            .control
+              button.button.is-primary.is-flip(:class="{ 'is-loading': isLoading }" type="submit")
+                span(data-text="Send message") Send message
 </template>
 
 <script>
@@ -146,11 +150,20 @@
           }
         ],
         isLoading: false,
+        isSuccess: false,
         isSignInError: false
       }
     },
 
     methods: {
+      error (err) {
+        this.isSignInError = err.message
+        this.isLoading = false
+        setTimeout(() => {
+          VueScrollTo.scrollTo('.is-error', { offset: -90 })
+        }, 300)
+      },
+
       submit () {
         // Hide errors first
         this.isSignInError = false
@@ -158,25 +171,31 @@
         this.$validator.validateAll()
           .then(response => {
             if (!response) {
-              this.isLoading = false
-              VueScrollTo.scrollTo('.is-error', { offset: -90 })
+              this.error({
+                error: response
+              })
               return
             }
             this.isLoading = true
 
-            api.contactForm.postData(this.formData)
+            return api.contactForm.postData(this.formData)
               .then(response => {
+                this.isLoading = false
                 if (response.status === 200) {
-                  this.isLoading = false
+                  this.isSuccess = true
+                  setTimeout(() => {
+                    VueScrollTo.scrollTo('.is-success', { offset: -90 })
+                  }, 300)
+                  return
                 }
+
+                this.error({
+                  message: 'Something went wrong'
+                })
               })
-          })
-          .catch(err => {
-            this.isSignInError = err.error
-            this.isLoading = false
-            setTimeout(() => {
-              VueScrollTo.scrollTo('.is-error', { offset: -90 })
-            }, 300)
+              .catch(err => {
+                this.error(err)
+              })
           })
       }
     }
@@ -189,17 +208,18 @@
   @import '~assets/css/utilities/variables.scss';
   @import '~assets/css/utilities/mixins.scss';
 
-  footer {
-    background-color: $grey;
-    position: relative;
-    background-image: url('/footer-bg.jpg');
-    background-size: cover;
+  .success-container {
+    background: $grey-300;
+    text-align: center;
+    padding: 50px;
 
-    .content {
-      p, strong, a {
-        color: $white;
-        font-size: rem(11px);
-      }
+    .title {
+      font-size: rem(22px);
+      @include ExtraBoldUppercase();
+    }
+
+    p {
+      font-size: rem(13px);
     }
   }
 </style>
